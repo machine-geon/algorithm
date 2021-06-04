58591639f33a38a85f9d3f2ab01fcd966e2db501
KimMingeon0625 <machine.kmg@gmail.com>
2021-06-05 02:05:41 +0900

    main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+	public static void main(String[] args) {
+		_20210605_KAKAO2021_신규아이디추천 day01 = new _20210605_KAKAO2021_신규아이디추천();
+		System.out.println(day01.solution("=.="));
+	}
+
+}
